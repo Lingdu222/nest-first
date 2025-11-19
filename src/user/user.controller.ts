5afc45957795e22e7c1deb751605afcfb8ce86df
zhangyuhong <jbzhy0424@163.com>
2025-11-19 19:28:00 +0800

    用typeorm操作数据库

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Controller('/user')
 export class UserController {
-  constructor(private readonly UserService: UserService) { }
+  constructor(private readonly UserService: UserService, @InjectRepository(User) private readonly userRepository: Repository<User>) { }
+
+
 
   @Get()
   getHello(): string {
+
+    // this.userRepository.save({ userName: 'A', password: 123, head_img: 'https://hellorfimg.zcool.cn/provider_image/large/hi2246794331.jpg?x-image-process=image/format,webp' }); // 示例：使用userRepository进行数据库操作
+    // 删除
+    // this.userRepository.delete({ userName: 'A' });
+    // 修改
+    // this.userRepository.update(3, { userName: 'B' }); 
+    // 查询
+    // this.userRepository.find(); 
     return this.UserService.getHello();
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
+
+// 引入TypeOrm模块
+// 引入User实体
+// User实体用于TypeOrm进行数据库操作
+// 所以需要在模块中引入TypeOrmModule.forFeature方法，并传入User实体
+// 这样UserService中就可以通过@InjectRepository(User)注入UserRepository进行数据库操作
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+
+
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })
