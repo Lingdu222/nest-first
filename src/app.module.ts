9dc82374f61ffb42d9efa10102f960d3d7f11057
zhangyuhong <jbzhy0424@163.com>
2025-11-20 10:37:36 +0800

    解决不能用快捷命令的问题

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,12 @@ import { ClassModule } from './class/class.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // 引入配置模块
 import { typeOrmConfig } from './config';
+import { User2Module } from './user2/user2.module';
+import { User3Module } from './user3/user3.module';
+import { User7Module } from './user7/user7.module';
 
 @Module({
-  imports: [UserModule, ClassModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [UserModule, ClassModule, TypeOrmModule.forRoot(typeOrmConfig), User2Module, User3Module, User7Module],
   controllers: [],
   providers: [],
 })
